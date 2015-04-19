45a6c7aecc525c5b75221040642967436ad72640
Manish Shukla <mshukla@usc.edu>
2015-04-18 21:01:40 -0700

    readding to github

---
+++ Cache/Cache/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Cache
+//
+//  Created by Manish Shukla on 4/15/15.
+//  Copyright (c) 2015 Sean Leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Cache/Cache/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Cache
+//
+//  Created by Manish Shukla on 4/15/15.
+//  Copyright (c) 2015 Sean Leonard. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
