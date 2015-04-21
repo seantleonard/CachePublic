9eea6eb04dd4ed80e659f95b9186147914663db9
Manish Shukla <mshukla@usc.edu>
2015-04-20 18:55:59 -0700

    working on views

--- Cache/Cache/ViewController.m
+++ Cache/Cache/ViewController.m
@@ -18,8 +18,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //UIImageView *backgroundView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background"]];
+    //[self.view addSubview:backgroundView];
     FBSDKLoginButton *loginButton = [[FBSDKLoginButton alloc] init];
-    loginButton.center = self.view.center;
+    //loginButton.center = self.view.center;
+    UIImageView *backgroundImageView = [[UIImageView alloc] initWithImage:[UIImage imageNamed:@"background"]];
+    [backgroundImageView setFrame:CGRectMake(0, 0, 380, 675)];
+    [self.view addSubview:backgroundImageView];
+    UIImageView *iconImageView = [[UIImageView alloc] init];
+    UIImage *iconImage = [UIImage imageNamed:@"sqre"];
+    iconImageView.frame = CGRectMake(75, 200, 250, 200);
+    [iconImageView setImage:iconImage];
+    [self.view addSubview:iconImageView];
+    loginButton.frame = CGRectMake(55, 500, 271, 37);
     [self.view addSubview:loginButton];
     PFObject *testObject = [PFObject objectWithClassName:@"TestObject"];
     testObject[@"foo"] = @"bar";
